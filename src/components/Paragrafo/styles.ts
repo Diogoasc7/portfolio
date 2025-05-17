27608e530216ad2464b60d6c2c39b03ebf8d1065
Diogoasc7 <diogodsc1456@gmail.com>
2025-05-17 00:05:16 -0300

    tema light e dark adicionados

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -2,8 +2,10 @@ import styled from 'styled-components'
 import { Props } from '.'
 
 export const P = styled.p<Props>`
-  font-size: ${(props) => (props.fontSize ? +'px' : '14px')};
-  color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
-    #282a35;
+  font-size: ${(props) => (props.fontSize ? `${props.fontSize}px` : '14px')};
+  color: ${(props) =>
+    props.tipo === 'principal'
+      ? props.theme.corPrincipal
+      : props.theme.corSecundaria};
   line-height: 22px;
 `
---
+++ src/themes/dark.ts
@@ -0,0 +1,17 @@
+const temDark = {
+  corPrincipal: 'white',
+  corSecundaria: 'white',
+  corDeFundo: '#282a35',
+  corDeFundoBotao: 'white',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temDark
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDeFundoBotao: string
+  corDaBorda: string
+}
