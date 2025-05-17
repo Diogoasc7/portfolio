1d277a565c16e8e3e7d4b82904873c8f4ac2ca2c
Diogoasc7 <diogodsc1456@gmail.com>
2025-05-16 22:56:07 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/diogoasc7.png" />
-    <Titulo fontSize={20}>Diogo Costa</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Diogo Costa</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        diogoasc7
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
